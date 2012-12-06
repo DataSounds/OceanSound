64b82640f5271d2237df996a37dcd6c05720f2ae
Luiz Irber <luiz.irber@gmail.com>
2012-12-06 14:47:15 -0200

    First implementation of mpl animation

--- src/OceanSound/visuals.py
+++ src/OceanSound/visuals.py
@@ -1,20 +1,18 @@
 #/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
 
 
-def plot_series(series, title, output, tstep=None):
+def plot_series(series, title, output):
     # line Series
     fig = plt.figure(0)#figsize=(12,5), dpi=300)
     fig.clf()
     fig.suptitle(title)
     fig.add_subplot(211)
     plt.plot(series, 'o-', linewidth=3)
-    if tstep:
-        bar = np.arange(0, np.nanmax(series), .1)
-        plt.plot([tstep/300.] * len(bar), bar, '--')
     x = np.arange(2002,2013,1)
     plt.xticks(np.arange(0,121,12),x)
 
@@ -23,8 +21,6 @@ def plot_series(series, title, output, tstep=None):
     plt.imshow(series_stacked)
     plt.gca().yaxis.set_visible(False)
     plt.xticks(np.arange(0,121,12),x)
-    if tstep:
-        pass #plt.plot()
     #plt.xlabel('Ano')
     #plt.ylabel(u'Concentração de Clorofila-a')
     #fig.savefig(output)
@@ -36,4 +32,46 @@ def plot_series(series, title, output, tstep=None):
     #plt.savefig('cor_pixeis_%s' % output)
 
 
+def plot_animation(series, title, output, t_max):
+    INTERVAL = 300
+    frames = int(t_max / float(INTERVAL))
 
+    fig = plt.figure(0)#figsize=(12,5), dpi=300)
+    fig.clf()
+
+    fig.suptitle(title)
+
+    # line Series
+    fig.add_subplot(211)
+    plt.plot(series, 'o-', linewidth=3)
+
+    bar = np.arange(0, np.nanmax(series), .1)
+    marker = plt.axvline(0, linestyle='--')
+
+    x = np.arange(2002,2013,1)
+    plt.xticks(np.arange(0,121,12),x)
+
+    # Colors
+    fig.add_subplot(212)
+    series_stacked = np.tile(series, (20,1))
+    plt.imshow(series_stacked)
+    plt.gca().yaxis.set_visible(False)
+    plt.xticks(np.arange(0,121,12),x)
+    #plt.xlabel('Ano')
+    #plt.ylabel(u'Concentração de Clorofila-a')
+    #fig.savefig(output)
+
+    #plt.savefig('cor_pixeis_%s' % output)
+
+    def init():
+        marker.set_data([0], marker.get_ydata())
+        return marker,
+
+    def animate(i):
+        print i
+        marker.set_data([i], marker.get_ydata())
+        return marker,
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=frames, interval=INTERVAL, blit=False)
+    plt.show()
