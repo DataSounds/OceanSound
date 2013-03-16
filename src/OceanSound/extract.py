674a706423eee4a0293171b67dfd413f5b139715
Luiz Irber <luiz.irber@gmail.com>
2013-03-16 14:28:42 -0700

    PEP8 fixes and removed some unused imports

--- src/OceanSound/extract.py
+++ src/OceanSound/extract.py
@@ -12,13 +12,12 @@ Baseado em subset_MODIS.py
 Author =  Arnaldo Russo
 e-mail =  arnaldorusso@gmail.com
 '''
-from pyhdf.SD import *
+
+
+from pyhdf.SD import SD
 import numpy as np
 import glob
 import os
-#import matplotlib.pyplot as plt
-#from mpl_toolkits.basemap import Basemap
-#import scipy.io
 
 
 def extract_series(LATLIMS, LONLIMS, indir):
@@ -27,42 +26,42 @@ def extract_series(LATLIMS, LONLIMS, indir):
 
     filelist = glob.glob(os.path.join(indir, 'A*'))
     filelist.sort()
-    nfiles = len(filelist)
     files = []
     for path in filelist:
-      files.append(path[len(indir):]) # remove path name
+        files.append(path[len(indir):])  # remove path name
 
     names = []
     multi_pix = []
     for i in range(len(filelist)):
         A = SD(filelist[i])
         a = A.attributes()
-        for k in xrange(0,len(a.keys())):
+        for k in xrange(0, len(a.keys())):
             nm = a.keys()[k]
-            names.append(nm.replace(" ","_"))
-        pin = dict(zip(names,a.values()[:]))
-        lon = np.arange(pin['Westernmost_Longitude'],pin['Easternmost_Longitude'],pin['Longitude_Step'])
-        lat = np.arange(pin['Northernmost_Latitude'],pin['Southernmost_Latitude'],-pin['Latitude_Step'])
+            names.append(nm.replace(" ", "_"))
+        pin = dict(zip(names, a.values()[:]))
+        lon = np.arange(pin['Westernmost_Longitude'], pin[
+                        'Easternmost_Longitude'], pin['Longitude_Step'])
+        lat = np.arange(pin['Northernmost_Latitude'], pin[
+                        'Southernmost_Latitude'], -pin['Latitude_Step'])
         # Get the indices needed for the area of interest
         # argmin catch the indices of minor element
-        ilt = np.int(np.argmin(np.abs(lat-np.max(LATLIMS))))
-        ilg = np.int(np.argmin(np.abs(lon-np.min(LONLIMS))))
+        ilt = np.int(np.argmin(np.abs(lat - np.max(LATLIMS))))
+        ilg = np.int(np.argmin(np.abs(lon - np.min(LONLIMS))))
         # retrieve data SDS
         d = A.datasets()
-        sds_name = d.keys()[0] # name of sds. Dictionary method.
+        sds_name = d.keys()[0]  # name of sds. Dictionary method.
         sds = A.select(sds_name)
         ## load the subset of data needed for the map limits given
-        P = sds[ilt,ilg]
+        P = sds[ilt, ilg]
         P = np.double(P)
         if P < pin['Data_Minimum']:
             P = np.nan
         #P[P<pin['Data_Minimum']] = np.nan
-        P = (pin['Slope']*P+pin['Intercept'])
-        Lat = lat[ilt] # lat[ilt+np.arange(0,ltlm-1)]
-        Lon = lon[ilg] # lon[ilg+np.arange(0,lglm-1)]
+        P = (pin['Slope'] * P + pin['Intercept'])
+        Lat = lat[ilt]  # lat[ilt+np.arange(0,ltlm-1)]
+        Lon = lon[ilg]  # lon[ilg+np.arange(0,lglm-1)]
         multi_pix.append(P)
-
 
     #multi_pix[multi_pix < pin['Data_Minimum']] = np.nan
     PAM = np.asarray(multi_pix)
-    return {"Series":PAM, "Lat":Lat, "Lon":Lon}
+    return {"Series": PAM, "Lat": Lat, "Lon": Lon}
--- src/OceanSound/visuals.py
+++ src/OceanSound/visuals.py
@@ -8,19 +8,19 @@ from matplotlib import animation
 
 def plot_series(series, title, output):
     # line Series
-    fig = plt.figure(0)#figsize=(12,5), dpi=300)
+    fig = plt.figure(0)  # figsize=(12,5), dpi=300)
     fig.clf()
     fig.suptitle(title)
     fig.add_subplot(211)
     plt.plot(series, 'o-', linewidth=3)
-    x = np.arange(2002,2013,1)
-    plt.xticks(np.arange(0,121,12),x)
+    x = np.arange(2002, 2013, 1)
+    plt.xticks(np.arange(0, 121, 12), x)
 
     fig.add_subplot(212)
-    series_stacked = np.tile(series, (20,1))
+    series_stacked = np.tile(series, (20, 1))
     plt.imshow(series_stacked)
     plt.gca().yaxis.set_visible(False)
-    plt.xticks(np.arange(0,121,12),x)
+    plt.xticks(np.arange(0, 121, 12), x)
     #plt.xlabel('Ano')
     #plt.ylabel(u'Concentração de Clorofila-a')
     #fig.savefig(output)
@@ -36,7 +36,7 @@ def plot_animation(series, title, output, t_max):
     INTERVAL = 200
     frames = int(t_max / float(INTERVAL))
 
-    fig = plt.figure(1)#figsize=(12,5), dpi=300)
+    fig = plt.figure(1)  # figsize=(12,5), dpi=300)
     fig.clf()
 
     fig.suptitle(title)
@@ -48,13 +48,13 @@ def plot_animation(series, title, output, t_max):
     bar = np.arange(0, np.nanmax(series), .1)
     marker = plt.axvline(0, linestyle='--')
 
-    x = np.arange(2002,2013,1)
+    x = np.arange(2002, 2013, 1)
     #plt.xticks(np.arange(0,121,12),x)
     plt.gca().xaxis.set_visible(False)
 
     # Colors
     fig.add_subplot(212)
-    series_stacked = np.tile(series, (20,1))
+    series_stacked = np.tile(series, (20, 1))
     plt.imshow(series_stacked)
     plt.gca().yaxis.set_visible(False)
     plt.gca().xaxis.set_visible(False)
@@ -71,11 +71,11 @@ def plot_animation(series, title, output, t_max):
 
     def animate(i):
         print i
-        marker.set_data([i * (120./frames)], marker.get_ydata())
+        marker.set_data([i * (120. / frames)], marker.get_ydata())
         return marker,
 
     print frames, INTERVAL, t_max
     anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=frames, interval=INTERVAL, blit=False)
+                                   frames=frames, interval=INTERVAL, blit=False)
     #plt.show()
     return anim
