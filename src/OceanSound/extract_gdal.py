21f73f415c39ac104650669d80525e9bf7e56cdd
arnaldorusso <arnaldorusso@gmail.com>
2013-08-02 15:09:30 -0300

    extract series using gdal

---
+++ src/OceanSound/extract_gdal.py
@@ -0,0 +1,61 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+subset_MODIS
+
+Programa para Fazer um subset teemporal de um pixel de imagens MODIS
+Dentro dos limites Lat e Long determinados.
+
+Baseado em subset_MODIS.py
+
+Author =  Arnaldo Russo
+e-mail =  arnaldorusso@gmail.com
+'''
+
+
+import gdal
+import numpy as np
+import glob
+import os
+
+
+def extract_series(LATLIMS, LONLIMS, indir):
+    LATLIMS = np.asarray(LATLIMS)
+    LONLIMS = np.asarray(LONLIMS)
+
+    filelist = glob.glob(os.path.join(indir, 'A*'))
+    filelist.sort()
+    files = []
+    for path in filelist:
+        files.append(path[len(indir):])  # remove path name
+
+    names = []
+    multi_pix = []
+    for i in range(len(filelist)):
+        A = None
+        a = None
+        A = gdal.Open(filelist[i],gdal.GA_ReadOnly)
+        a = A.GetMetadata()
+        lon = np.arange(np.float(a['Westernmost Longitude']), np.float(a[
+                        'Easternmost Longitude']), np.float(a['Longitude Step']))
+        lat = np.arange(np.float(a['Northernmost Latitude']), np.float(a[
+                        'Southernmost Latitude']), -np.float(a['Latitude Step']))
+        # Get the indices needed for the area of interest
+        # argmin catch the indices of minor element
+        ilt = np.int(np.argmin(np.abs(lat - np.max(LATLIMS))))
+        ilg = np.int(np.argmin(np.abs(lon - np.min(LONLIMS))))
+        ## load the subset of data needed for the map limits given
+        P = A.ReadAsArray()[ilt, ilg]
+        P = np.double(P)
+        if P < np.float(a['Data Minimum']):
+            P = np.nan
+        #P[P<pin['Data_Minimum']] = np.nan
+        P = (np.float(a['Slope']) * P + np.float(a['Intercept']))
+        Lat = lat[ilt]  # lat[ilt+np.arange(0,ltlm-1)]
+        Lon = lon[ilg]  # lon[ilg+np.arange(0,lglm-1)]
+        multi_pix.append(P)
+
+    #multi_pix[multi_pix < pin['Data_Minimum']] = np.nan
+    PAM = np.asarray(multi_pix)
+    return {"Series": PAM, "Lat": Lat, "Lon": Lon}
