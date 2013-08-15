c8fa615237c2191c691f94537658b91b3400a8c5
Luiz Irber <luiz.irber@gmail.com>
2013-08-15 00:00:24 -0300

    Added a test

---
+++ tests/test_extract.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+try:
+    from urllib.request import urlretrieve
+except ImportError:
+    from urllib import urlretrieve
+
+
+from OceanSound.extract_pyhdf import extract_series as pyhdf_extract
+from OceanSound.extract_gdal import extract_series as gdal_extract
+
+
+CONTAINER = "http://41e7942089bad530cb53-26c284b979cc7ba85c89e77b5734ccd5.r9.cf2.rackcdn.com/"
+
+TEST_FILES = (
+  "A20021822002212.L3m_MO_CHL_chlor_a_9km",
+  "A20022132002243.L3m_MO_CHL_chlor_a_9km"
+)
+
+
+def test_extract(tmpdir):
+    lat, lon = 0, 0
+    indir = str(tmpdir)
+    for test_file in TEST_FILES:
+        urlretrieve(CONTAINER + test_file, str(tmpdir.join(test_file)))
+    assert pyhdf_extract(lat, lon, indir) == gdal_extract(lat, lon, indir)
