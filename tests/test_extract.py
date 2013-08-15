3384902521fc8db8ce103fa1429e580b5f842a5d
arnaldorusso <arnaldorusso@gmail.com>
2013-08-15 10:54:21 -0300

    Testing length of extracted data

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -24,3 +24,12 @@ def test_extract(tmpdir):
     for test_file in TEST_FILES:
         urlretrieve(CONTAINER + test_file, str(tmpdir.join(test_file)))
     assert pyhdf_extract(lat, lon, indir) == gdal_extract(lat, lon, indir)
+
+def test_extracted(tmpdir):
+    lat, lon = 0, 0
+    indir = str(tmpdir)
+    for test_file in TEST_FILES:
+        urlretrieve(CONTAINER + test_file, str(tmpdir.join(test_file)))
+    a_pyhdf = pyhdf_extract(lat, lon, indir)
+    a_gdal = gdal_extract(lat, lon, indir)
+    assert len(a_pyhdf['Series']) == len(TEST_FILES) == len(a_gdal['Series'])
