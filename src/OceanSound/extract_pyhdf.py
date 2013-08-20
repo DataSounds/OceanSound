055b20656c070afd1f8d0734d7d655f4b32813e2
Luiz Irber <luiz.irber@gmail.com>
2013-08-19 23:02:16 -0300

    Simplify extract_pyhdf

--- src/OceanSound/extract_pyhdf.py
+++ src/OceanSound/extract_pyhdf.py
@@ -4,7 +4,7 @@
 '''
 subset_MODIS using pyhdf package.
 
-Makes a subset of a pixel time series from MODIS 
+Makes a subset of a pixel time series from MODIS
 Chlorophyll images.
 '''
 
@@ -19,27 +19,18 @@ def extract_series(LATLIMS, LONLIMS, indir):
     LATLIMS = np.asarray(LATLIMS)
     LONLIMS = np.asarray(LONLIMS)
 
-    filelist = glob.glob(os.path.join(indir, 'A*'))
-    filelist.sort()
-    files = []
-    for path in filelist:
-        files.append(path[len(indir):])  # remove path name
-
-    names = []
     multi_pix = []
     Lat = None
     Lon = None
-    for i in range(len(filelist)):
-        A = SD(filelist[i])
-        a = A.attributes()
-        for k in xrange(0, len(a.keys())):
-            nm = a.keys()[k]
-            names.append(nm.replace(" ", "_"))
-        pin = dict(zip(names, a.values()[:]))
-        lon = np.arange(pin['Westernmost_Longitude'], pin[
-                        'Easternmost_Longitude'], pin['Longitude_Step'])
-        lat = np.arange(pin['Northernmost_Latitude'], pin[
-                        'Southernmost_Latitude'], -pin['Latitude_Step'])
+    for filename in sorted(glob.glob(os.path.join(indir, 'A*'))):
+        A = SD(filename)
+        pin = A.attributes()
+        lon = np.arange(pin['Westernmost Longitude'],
+                        pin['Easternmost Longitude'],
+                        pin['Longitude Step'])
+        lat = np.arange(pin['Northernmost Latitude'],
+                        pin['Southernmost Latitude'],
+                        -pin['Latitude Step'])
         # Get the indices needed for the area of interest
         # argmin catch the indices of minor element
         ilt = np.int(np.argmin(np.abs(lat - np.max(LATLIMS))))
@@ -49,16 +40,12 @@ def extract_series(LATLIMS, LONLIMS, indir):
         sds_name = d.keys()[0]  # name of sds. Dictionary method.
         sds = A.select(sds_name)
         ## load the subset of data needed for the map limits given
-        P = sds[ilt, ilg]
-        P = np.double(P)
-        if P < pin['Data_Minimum']:
+        P = np.double(sds[ilt, ilg])
+        if P < pin['Data Minimum']:
             P = np.nan
-        #P[P<pin['Data_Minimum']] = np.nan
-        P = (pin['Slope'] * P + pin['Intercept'])
-        Lat = lat[ilt]  # lat[ilt+np.arange(0,ltlm-1)]
-        Lon = lon[ilg]  # lon[ilg+np.arange(0,lglm-1)]
+        P = pin['Slope'] * P + pin['Intercept']
+        Lat = lat[ilt]
+        Lon = lon[ilg]
         multi_pix.append(P)
 
-    #multi_pix[multi_pix < pin['Data_Minimum']] = np.nan
-    PAM = np.asarray(multi_pix)
-    return {"Series": PAM, "Lat": Lat, "Lon": Lon}
+    return {"Series": np.asarray(multi_pix), "Lat": Lat, "Lon": Lon}
